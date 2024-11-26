214a4429b2fc71abe72b9d7af393d2ebc23c5124
Simon Villiard <villiard94@gmail.com>
2024-11-25 20:24:28 -0500

    stylelint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,33 +1,33 @@
 /* eslint-disable import/no-named-as-default-member */
-import pluginJs from '@eslint/js';
-import pluginPrettier from 'eslint-plugin-prettier/recommended';
-import pluginSolid from 'eslint-plugin-solid';
-import globals from 'globals';
-import pluginTs from 'typescript-eslint';
-import pluginImport from 'eslint-plugin-import';
+import pluginJs from "@eslint/js";
+import pluginPrettier from "eslint-plugin-prettier/recommended";
+import pluginSolid from "eslint-plugin-solid";
+import globals from "globals";
+import pluginTs from "typescript-eslint";
+import pluginImport from "eslint-plugin-import";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  { ignores: ['**/*/node_modules', '**/*/dist'] },
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { ignores: ["**/*/node_modules", "**/*/dist"] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...pluginTs.configs.recommended,
-  pluginSolid.configs['flat/recommended'],
+  pluginSolid.configs["flat/recommended"],
   pluginImport.flatConfigs.recommended,
   pluginImport.flatConfigs.typescript,
   pluginPrettier,
   {
     rules: {
-      'import/no-unresolved': 'off',
-      'import/named': 'off',
-      'import/order': [
-        'error',
-        { groups: ['external', 'builtin', 'internal', 'sibling', 'parent', 'index'] },
+      "import/no-unresolved": "off",
+      "import/named": "off",
+      "import/order": [
+        "error",
+        { groups: ["external", "builtin", "internal", "sibling", "parent", "index"] },
       ],
     },
     settings: {
-      'import/resolver': {
+      "import/resolver": {
         typescript: true,
         node: true,
       },
